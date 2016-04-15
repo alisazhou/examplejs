20248e68a24b303e9f991bedf7c1357e469f1a49
conrad <conrad@pythonanywhere.com>
2016-04-15 22:00:07 +0000

    add currently useless redux store

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 // import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
 import StickyLayout from './components/sticky-layout/StickyLayout.jsx';
 
+// a redux reducer reacts/responds to actions by chging the state
+// const reducer1 = (previousState, action) => Object.assign({}, previousState);
+// combineReducers calls all reducers for all actions but chunks up state
+// if you had state.a, state.b, state.c
+// combineReducers({a: reducerThatWillChangeStateA, ... })
+// so an action should contain all the info needed to chg states a and or b and or c if necessary
+const rootReducer = (state, action) => state;
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <StickyLayout/>,
+  <Provider store={store}>
+    <StickyLayout/>
+  </Provider>,
   document.getElementById('reactApp')
 );
