23dab57f472c870e067efd96d278380bc0d9b8b2
conrad <conrad@pythonanywhere.com>
2016-04-29 17:42:06 +0000

    add seller name into component with tests

--- frontend/src/components/seller/Seller.jsx
+++ frontend/src/components/seller/Seller.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 
 class Seller extends React.Component {
-  render () { return <div><p/></div>; }
+  render () { return <div>{this.props.name}</div>; }
 }
+
+Seller.propTypes = {name: React.PropTypes.string.isRequired};
+
 export default Seller;
--- frontend/src/components/seller/Seller.spec.js
+++ frontend/src/components/seller/Seller.spec.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
+import R from 'ramda';
 
 jest.dontMock('./Seller.jsx');
 const sellerModule = require('./Seller.jsx');
@@ -9,12 +10,16 @@ const Seller = sellerModule.default;
 
 describe('Seller react component', () => {
   const shallowRenderer = TestUtils.createRenderer();
-  shallowRenderer.render(
-    <Seller/>
-  );
+  shallowRenderer.render(<Seller name='sunny'/>);
   const result = shallowRenderer.getRenderOutput();
 
   it('renders to a div', () => {
     expect(result.type).toBe('div');
+  });
+  it('renders props.name', () => {
+    expect(result.props.children).toContain('sunny');
+  });
+  it('has correct propTypes', () => {
+    expect(R.has('name')(Seller.propTypes)).toBe(true);
   });
 });
