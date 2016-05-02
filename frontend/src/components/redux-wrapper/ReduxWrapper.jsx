2e5347a10f176e67103fb811f488cbcdf212c95e
conrad <conrad@pythonanywhere.com>
2016-05-02 13:08:10 +0100

    add redux-logger middleware

--- frontend/src/components/redux-wrapper/ReduxWrapper.jsx
+++ frontend/src/components/redux-wrapper/ReduxWrapper.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
 
 import currentPage from '../sticky-layout/currentPageReducer.js';
 import sellers from '../seller/sellersReducer.js';
 import currentSeller from '../seller/currentSellerReducer.js';
 
 const rootReducer = combineReducers({ currentPage, sellers, currentSeller });
-export let store = createStore(rootReducer);
+const logger = createLogger();
+export let store = createStore(rootReducer, applyMiddleware(logger));
 
 export default function wrapComponent (Component) {
   class WrappedComponent extends React.Component {
