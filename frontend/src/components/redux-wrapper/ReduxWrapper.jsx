269b61c4e7e38de8a5d27c159e5f267e7f6e0829
conrad <conrad.alwin.ho@gmail.com>
2016-05-22 16:59:28 +0100

    turn off redux-logging so that redux-form doesn't excitedly poop everywhere whenever touched

--- frontend/src/components/redux-wrapper/ReduxWrapper.jsx
+++ frontend/src/components/redux-wrapper/ReduxWrapper.jsx
@@ -6,17 +6,24 @@ import createLogger from 'redux-logger';
 import currentPage from '../sticky-layout/currentPageReducer.js';
 import sellers from '../seller/sellersReducer.js';
 import currentSeller from '../seller/currentSellerReducer.js';
-import { reducer as formReducer } from 'redux-form';
+import { reducer as form } from 'redux-form';
 
 
 const rootReducer = combineReducers({
   currentPage,
   sellers,
   currentSeller,
-  form: formReducer
+  form,
 });
-const logger = createLogger();
-export let store = createStore(rootReducer, applyMiddleware(logger));
+
+const middlewares = [];
+
+// jest test runner node_env
+if (process.env.NODE_ENV !== 'test') {
+  const logger = createLogger();
+  middlewares.push(logger);
+}
+export let store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default function wrapComponent (Component) {
   class WrappedComponent extends React.Component {
