fd97e2a367ba0f2c6864c0b17f3e752e1a38002e
alisazzz <alisa.zwy@gmail.com>
2016-05-24 17:57:13 -0400

    ft for storing booking info in redux-form

--- integration_tests/navigation_test.py
+++ integration_tests/navigation_test.py
@@ -1,4 +1,3 @@
-
 def test_can_use_progress_bar_to_navigate(browser):
     browser.goto('/')
     # click on button inside of intro page
@@ -13,4 +12,24 @@ def test_can_use_progress_bar_to_navigate(browser):
     assert 'Reservation Details' in browser.body_text
 
 def test_saves_state_between_pages():
-    pass
+    browser.goto('/')
+    # click on button inside of intro page
+    browser.find_element_by_xpath('//button[text()[contains(.,"Book Now")]]').click()
+    booking = {
+        "name": "test_name",
+        "tel": "test_tel",
+        "address": "test_address",
+        "time": "test_time",
+    }
+    # no booking info displayed before filling in form
+    assert 'test_name' not in browser.body_text
+
+    # fill in reservation form
+    for field, value in user.items():
+        browser.find_element_by_name(field).send_keys(value)
+    assert 'test_name' in browser.body_text
+
+    # go to next page and back again, booking info is displayed
+    browser.find_element_by_xpath('//div[text()="2. Choose Available People"]').click()
+    browser.find_element_by_xpath('//div[text()="1. Booking Details"]').click()
+    assert 'test_name' in browser.body_text
