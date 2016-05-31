168ab0004f4cc070fe5fba41156999b2e5d876d5
Alisa Zhou <alisa.zwy@gmail.com>
2016-05-31 12:28:08 -0400

    move from browserify to webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+  entry: "./src/app.jsx",
+  output: {
+    path: "../static/", 
+    filename: "bundled.js",
+    sourceMapFilename: "bundled.map"
+  },
+  devtool: "#source-map",
+  module: {
+    resolve: {
+      extensions: [ '', 'js', 'jsx' ]
+    },
+    loaders: [
+      {
+          test: /\.jsx?$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+          query: {
+              presets: ['es2015', 'react']
+          }
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+  ]
+};
