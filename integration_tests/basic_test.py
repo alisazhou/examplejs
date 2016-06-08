3f690e2521e8a24444d6d1d235d030d21de26dd8
conrad <conrad.alwin.ho@gmail.com>
2016-06-08 20:26:06 +0100

    reorganize and rename fts

--- integration_tests/basic_test.py
+++ integration_tests/basic_test.py
@@ -5,6 +5,6 @@ def test_static_files_are_served(live_server):
     response = requests.get(live_server.url + '/static/tests/testy.js')
     assert response.status_code == 200
 
-def test_index_page_executes_react_script_correctly(browser):
+def test_react_script_loads_successfully_for_index_page(browser):
     assert 'Loading...' not in browser.body_text
     assert 'iChef' in browser.body_text
