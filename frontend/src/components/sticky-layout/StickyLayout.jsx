58e8429814b0aef9ea89ce0f8daeed9216fe9825
Alisa Zhou <alisa.zwy@gmail.com>
2016-06-09 17:01:30 -0400

    unlink StickyLayout from BaseChangePage

--- frontend/src/components/sticky-layout/StickyLayout.jsx
+++ frontend/src/components/sticky-layout/StickyLayout.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
-import { connect } from 'react-redux';
 
-
-import { pageMapping } from './pageMapping.js';
 import Footer from '../footer/Footer.jsx';
+import IntroPage from '../intro-page/IntroPage.jsx';
 
-class StickyLayout extends React.Component {
+export default class StickyLayout extends React.Component {
   // pure render
   shouldComponentUpdate (nextProps, nextState) {
     return shallowCompare(this, nextProps, nextState);
   }
 
   render () {
-    const CurrentPage = pageMapping.get(this.props.currentPage);
     return (
       <div>
         <div className='sticky-layout--body'>
-          <CurrentPage/>
+          <IntroPage/>
         </div>
         <div className='sticky-layout--footer'>
           <Footer/>
@@ -26,13 +23,3 @@ class StickyLayout extends React.Component {
     );
   }
 }
-
-StickyLayout.propTypes = {
-  currentPage: React.PropTypes.string.isRequired,
-};
-
-const mapStateToProps = state => ({
-  currentPage: state.currentPage,
-});
-export default connect(mapStateToProps, null)(StickyLayout);
-export { pageMapping, StickyLayout };
