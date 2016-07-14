56865734655c8dc160b387efe6c9c9aafd1e0a49
Alisa Zhou <alisa.zwy@gmail.com>
2016-07-14 14:19:09 -0400

    refactor to use selenium Select -housekeeping

--- integration_tests/ui_tests/components.py
+++ integration_tests/ui_tests/components.py
@@ -2,8 +2,3 @@
 def click_on_nth_menu(browser, nth):
     ### HACK
     browser.click_link_id('menu_{}'.format(nth))
-
-def click_on_option_by_text(opt_list, opt_text):
-    for opt in opt_list:
-        if opt.text == opt_text:
-            opt.click()
