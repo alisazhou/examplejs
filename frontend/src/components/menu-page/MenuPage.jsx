09e7a907733fefcd7d5a13bfd5d0f9f61e332433
Alisa Zhou <alisa.zwy@gmail.com>
2016-07-15 16:34:13 -0400

    updates order with menu id from MenuPage

--- frontend/src/components/menu-page/MenuPage.jsx
+++ frontend/src/components/menu-page/MenuPage.jsx
@@ -5,6 +5,7 @@ import R from 'ramda';
 
 import MenuDescription from './MenuDescription.jsx';
 import OrderAttributes from './OrderAttributes.jsx';
+import { updateOrderActionCreator } from '../../actions/orderActions.js';
 
 
 class MenuPage extends React.Component {
@@ -14,16 +15,15 @@ class MenuPage extends React.Component {
         <h1>{this.props.menu.name}</h1>
         <MenuDescription {...this.props.menu}/>
         <OrderAttributes />
-        <Link to='/reservation'>Next</Link>
+        <Link to='/reservation'>
+          <button onClick={this.props.updateOrder}>Next</button>
+        </Link>
       </div>
     );
   }
 }
 
 MenuPage.propTypes = {
-  params: React.PropTypes.shape({
-    menuId: React.PropTypes.string.isRequired,
-  }),
   menu: React.PropTypes.shape({
     id: React.PropTypes.string.isRequired,
     category: React.PropTypes.string.isRequired,
@@ -32,11 +32,21 @@ MenuPage.propTypes = {
     description: React.PropTypes.string.isRequired,
     image: React.PropTypes.string.isRequired,
   }),
+  params: React.PropTypes.shape({
+    menuId: React.PropTypes.string.isRequired,
+  }),
+  updateOrder: React.PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => ({
   menu: R.find(R.propEq('id', ownProps.params.menuId))(state.menus),
 });
 
-export default connect(mapStateToProps)(MenuPage);
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  updateOrder: () => {
+    dispatch(updateOrderActionCreator({ menuId: ownProps.params.menuId }));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MenuPage);
 export { MenuPage };
