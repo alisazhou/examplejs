117ec3a715bc6a2c2271e59ce6cee4da369e07a9
conrad <conrad.alwin.ho@gmail.com>
2016-07-17 15:40:14 +0100

    add navbar scss and chg Navbar.jsx to use it

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -9,7 +9,13 @@ class Navbar extends React.Component {
   render () {
     return (
       <div className='navbar'>
-      { this.props.isAuthenticated ? <ProfileButton /> : <AuthBlock /> }
+        <div className='navbar--buttons'>
+          <div className='navbar--buttons--home'>
+            <button type='button'>iChef</button>
+          </div>
+          <div className='navbar--buttons--title'/>
+          { this.props.isAuthenticated ? <ProfileButton className='navbar--buttons--login'/> : <AuthBlock className='navbar--buttons--login'/> }
+        </div>
       </div>
     );
   }
