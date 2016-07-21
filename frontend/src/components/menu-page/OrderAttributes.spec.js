ac25ac4b92c49b9a0709cb7499ef57895a05aedf
Alisa Zhou <alisa.zwy@gmail.com>
2016-07-21 15:56:43 -0400

    add default label to OrderAttributes select tag

--- frontend/src/components/menu-page/OrderAttributes.spec.js
+++ frontend/src/components/menu-page/OrderAttributes.spec.js
@@ -23,16 +23,16 @@ describe('OrderAttributes dumb component', () => {
     expect(result.type).toBe('form');
   });
 
-  it('has a select child component with 5 options', () => {
+  it('has a select child component with 6 options', () => {
     const firstLabel = result.props.children[0];
     const select = R.find(R.propEq('type', 'select'))(firstLabel.props.children);
     expect(select).toBeDefined();
     const options = select.props.children.filter(child =>
       child.type === 'option'
     );
-    expect(options.length).toEqual(5);
+    expect(options.length).toEqual(6);
   });
-  
+
   it('has an input child', () => {
     const secondLabel = result.props.children[1];
     const input = R.find(R.propEq('type', 'input'))(secondLabel.props.children);
