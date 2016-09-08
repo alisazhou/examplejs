920a9182c95a1d2e5f9a2f76f9f80d0dcf1ad4a7
conrad <conrad.alwin.ho@gmail.com>
2016-09-08 09:17:37 +0100

    fix todo: use beforeEach but take into account nested describe/beforeEach interaction

--- frontend/src/components/formHelpers.spec.js
+++ frontend/src/components/formHelpers.spec.js
@@ -3,42 +3,57 @@ import { validateAndFindUntouched } from './formHelpers.js';
 
 
 describe('validateAndFindUntouched form helper', () => {
+  let spy, mockEvent;
+  beforeEach(() => {
+    spy = jasmine.createSpy('spy');
+    mockEvent = { preventDefault: spy };
+  });
 
   describe('when all fields are true', () => {
-    // TODO: beforeEach doesn't work???
-    const spy = jasmine.createSpy('spy');
-    let mockEvent = { preventDefault: spy};
-    const fieldsStatus = { fieldAValidated: true, fieldBValidated: true };
-    const untouched = validateAndFindUntouched(mockEvent, fieldsStatus);
+    let fieldsStatus, untouched;
+    beforeEach(() => {
+      fieldsStatus = { fieldAValidated: true, fieldBValidated: true };
+      untouched = validateAndFindUntouched(mockEvent, fieldsStatus);
+    });
+
     it('does not prevent default', () => {
       expect(mockEvent.preventDefault).not.toHaveBeenCalled();
     });
+
     it('finds no untouched', () => {
       expect(untouched).toEqual([]);
     });
   });
+
 
   describe('when some field is false', () => {
-    const spy = jasmine.createSpy('spy');
-    let mockEvent = { preventDefault: spy};
-    const fieldsStatus = { fieldAValidated: false, fieldBValidated: true };
-    const untouched = validateAndFindUntouched(mockEvent, fieldsStatus);
+    let fieldsStatus, untouched;
+    beforeEach(() => {
+      fieldsStatus = { fieldAValidated: false, fieldBValidated: true };
+      untouched = validateAndFindUntouched(mockEvent, fieldsStatus);
+    });
+
     it('prevents default', () => {
       expect(mockEvent.preventDefault).toHaveBeenCalled();
     });
+
     it('finds no untouched', () => {
       expect(untouched).toEqual([]);
     });
   });
 
+
   describe('when some field is undefined', () => {
-    const spy = jasmine.createSpy('spy');
-    let mockEvent = { preventDefault: spy};
-    const fieldsStatus = { fieldAValidated: undefined, fieldBValidated: true };
-    const untouched = validateAndFindUntouched(mockEvent, fieldsStatus);
+    let fieldsStatus, untouched;
+    beforeEach(() => {
+      fieldsStatus = { fieldAValidated: undefined, fieldBValidated: true };
+      untouched = validateAndFindUntouched(mockEvent, fieldsStatus);
+    });
+
     it('prevents default', () => {
       expect(mockEvent.preventDefault).toHaveBeenCalled();
     });
+
     it('finds untouched', () => {
       expect(untouched).toEqual([ 'fieldA' ]);
     });
