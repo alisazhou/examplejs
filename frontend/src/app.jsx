bb31730472bfb0aa757c942bf072ad66f63539c6
conrad <conrad.alwin.ho@gmail.com>
2016-09-16 14:57:52 +0100

    take out nodep-date-input-polyfill

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import WrappedLayout from './components/redux-wrapper/ReduxWrapper.jsx';
 
 import 'babel-polyfill';
-import 'nodep-date-input-polyfill';
 
 ReactDOM.render(
   <WrappedLayout/>,
