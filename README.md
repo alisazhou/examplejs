2e73e2e78d8066715c293afca5f1ca62f4a9e1e4
conrad <conrad.alwin.ho@gmail.com>
2016-09-17 17:38:01 +0100

    minor css tweaks

--- README.md
+++ README.md
@@ -36,6 +36,8 @@ See individual folders for their READMEs
 - [ui] make datetime captured in OrderAttributes more readable on reservation page
 - get title to change depending on react routing
 - instead of xs-hide, sm-hide, md-hide etc, maybe have a function that hides if less than lg etc
+- ask arun:
+    - login button slightly too close to right hand side
 
 
 **DOING**
--- integration_tests/appearance_test.py
+++ integration_tests/appearance_test.py
@@ -5,6 +5,7 @@ from browser_fixture import BaseBrowser, setup_pytest_browser_fixture
 
 class AppearnceTestBrowser(NavbarMixin, BaseBrowser):
     pass
+
 browser = setup_pytest_browser_fixture(AppearnceTestBrowser)
 
 def test_viewport_info_used_for_responsive_css(browser):
@@ -15,4 +16,3 @@ def test_viewport_info_used_for_responsive_css(browser):
     browser.set_window_size(10, 10)
     # she does not see the signup button
     browser.assert_is_not_visible(By.CSS_SELECTOR, 'button.navbar-buttons__user-signup-btn')
-    browser.set_window_size(1200, 800)
