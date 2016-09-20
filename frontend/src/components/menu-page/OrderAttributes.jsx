37dcdda52a90408167998d9e5dac8231501e6915
alisazzz <alisa.zwy@gmail.com>
2016-09-19 23:42:12 -0400

    spike order attrs

--- frontend/src/components/menu-page/OrderAttributes.jsx
+++ frontend/src/components/menu-page/OrderAttributes.jsx
@@ -1,84 +1,63 @@
 import React from 'react';
 import R from 'ramda';
-import { reduxForm, Field, formValueSelector } from 'redux-form';
+import { formValueSelector, isValid, touch } from 'redux-form';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
 
-import partySizeOptions from './orderAttributesConstants.js';
-import { renderInput, renderSelect } from '../formHelpers.js';
+import SearchDate from '../search-bar/SearchDate.jsx';
+import SearchSize from '../search-bar/SearchSize.jsx';
 import { updateOrderActionCreator } from '../../actions/orderActions.js';
 
 
-const validate = values => {
-  let errors = {};
-  if (!values.partySize || values.partySize === 'Number of guests') {
-    errors.partySize = 'Please select the number of guests.';
-  }
-  if (!values.dateTime) {
-    errors.dateTime = 'Please specify a time.';
-  }
-  return errors;
-};
-
 class OrderAttributes extends React.Component {
   render () {
-    const { handleSubmit } = this.props;
-    const onMenuNextClick = data => {
-      const updates = {...data, menuId: this.props.menuId};
+    const onNextClick = () => {
+      const { dateTime, menuId, partySize } = this.props;
+      const updates = { dateTime, partySize, menuId };
+      this.props.touchAll();
       this.props.updateOrder(updates);
-      browserHistory.push('/reservation/');
+      if (this.props.formsValid) {
+        browserHistory.push('/reservation/');
+      }
     };
 
     return (
-      <form
-        className='order-form'
-        onSubmit={handleSubmit(onMenuNextClick)}
-      >
-        <Field component={renderSelect}
-          name='partySize'
-          options={partySizeOptions}
-          className='order-form__field'
-        />
-        <Field component={renderInput}
-          name='dateTime'
-          type='date'
-          className='order-form__field'
-        />
-        <button type='submit' className='order-form__button'>
-          Order
-        </button>
-      </form>
+      <div>
+        <SearchDate displayError={true} />
+        <SearchSize displayError={true} />
+        <button onClick={onNextClick}>Next</button>
+      </div>
     );
   }
 }
 
 OrderAttributes.propTypes = {
-  handleSubmit: React.PropTypes.func.isRequired,
-  initialValues: React.PropTypes.shape({
-    dateTime: React.PropTypes.string,
-  }),
-  menuId: React.PropTypes.string.isRequired,
+  dateTime: React.PropTypes.string,
+  formsValid: React.PropTypes.bool.isRequired,
+  menuId: React.PropTypes.string,
+  partySize: React.PropTypes.string,
+  touchAll: React.PropTypes.func.isRequired,
   updateOrder: React.PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => {
-  const selector = formValueSelector('searchBar');
-  const dateTime = selector(state, 'searchDate');
-  return { initialValues: { dateTime }};
+  const dateTime = formValueSelector('searchDate')(state, 'searchDate');
+  const partySize = formValueSelector('searchSize')(state, 'searchSize');
+  const formsValid = isValid('searchDate')(state) && isValid('searchSize')(state);
+  return {dateTime, formsValid, partySize};
 };
 const mapDispatchToProps = dispatch => ({
+  touchAll: () => {
+    // first arg is form name, second arg is field name
+    dispatch(touch('searchDate', 'searchDate'));
+    dispatch(touch('searchSize', 'searchSize'));
+  },
   updateOrder: R.compose(dispatch, updateOrderActionCreator),
 });
 
-const FormConnectedAttributes = reduxForm({
-  form: 'orderAttributes',
-  destroyOnUnmount: false,
-  validate,
-})(OrderAttributes);
-
 const ReduxConnectedAttributes = connect(
   mapStateToProps, mapDispatchToProps
-)(FormConnectedAttributes);
+)(OrderAttributes);
 
 export default ReduxConnectedAttributes;
-export { FormConnectedAttributes, OrderAttributes };
+export { OrderAttributes };
